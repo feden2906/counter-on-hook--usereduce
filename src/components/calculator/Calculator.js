37ed85b863c2d0406262ed22dc216148f5e7d697
Fedenko Maksym <feden2906@gmail.com>
2020-12-25 14:38:47 +0200

    create store and reducer

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -4,53 +4,8 @@ import Title from "./title/Title";
 import Result from "./result/Result";
 import BtnWrapper from "./btnWrapper/BtnWrapper";
 import InputWrapper from "./inputWrapper/InputWrapper";
-
-const reducer = (state, action) => {
-  const {result, inputValue} = state
-  switch (action.type) {
-    case "PLUS_ONE": {
-      return {
-        ...state,
-        result: result + 1
-      }
-    }
-    case "PLUS_HUNDRED": {
-      return {result: result + 100}
-    }
-    case "MINUS_ONE": {
-      if (result - 1 < 0) {
-        return {result: 0}
-      } else {
-        return {result: result - 1}
-      }
-    }
-    case "MINUS_HUNDRED": {
-      if (result - 100 < 0) {
-        return {result: 0}
-      } else {
-        return {result: result - 100}
-      }
-    }
-    case "RESET": {
-      return {result: 0}
-    }
-    case "SUBMIT_NUMBER": {
-      if (isNaN(inputValue) || action.payload === '') {
-        return {...state, showError: true}
-      }
-      return {...state, result: result + inputValue, inputValue: ''}
-    }
-    case "SET_VALUE_REACT_INPUT": {
-        return {...state, inputValue: action.payload, showError: false}
-      }
-    default: {
-      console.log(`ERROR! Action ${action.type} not found.`)
-      return state
-    }
-  }
-}
-
-const initialState = {result: 0, showError: false, inputValue: ''}
+import initialState from '../../store/initialState';
+import reducer from '../../reducer/reducer';
 
 export default function Calculator() {
   const [state, dispatch] = useReducer(reducer, initialState)
--- src/components/calculator/btnWrapper/BtnWrapper.js
+++ src/components/calculator/btnWrapper/BtnWrapper.js
@@ -4,10 +4,10 @@ import './BtnWrapper.css'
 export default function BtnWrapper({onClickButton}) {
   return (
       <div className='btn-wrapper'>
-        <button onClick={() => onClickButton("PLUS_ONE")} className='btn'>+ 1</button>
-        <button onClick={() => onClickButton("PLUS_HUNDRED")} className='btn'>+ 100</button>
-        <button onClick={() => onClickButton("MINUS_ONE")} className='btn'>- 1</button>
-        <button onClick={() => onClickButton("MINUS_HUNDRED")} className='btn'>- 100</button>
+        <button onClick={() => onClickButton("MULTIPLY", 1)} className='btn'>+ 1</button>
+        <button onClick={() => onClickButton("MULTIPLY", 100)} className='btn'>+ 100</button>
+        <button onClick={() => onClickButton("MULTIPLY", -1)} className='btn'>- 1</button>
+        <button onClick={() => onClickButton("MULTIPLY", -100)} className='btn'>- 100</button>
         <button onClick={() => onClickButton("RESET")} className='btn'>Reset</button>
       </div>
   );
