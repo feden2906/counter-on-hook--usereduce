2d08ee78b82048179baee6d1225d8ca30bd03bac
freefireman <beluh.c.s@gmail.com>
2020-12-25 14:52:45 +0200

    pretty style

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,35 +1,42 @@
 const reducer = (state, action) => {
-  const {result, inputValue} = state
-  const {type, payload} = action
-  switch (type) {
-    case "MULTIPLY": {
-      if (result + payload > 0) {
-        return {...state, result: result + payload}
-      }else {
-        return {...state, result: 0}
-      }
+
+    const {result, inputValue} = state
+    const {type, payload} = action
+
+    switch (type) {
+
+        case "MULTIPLY": {
+
+            return (result + payload > 0)
+                ? {...state, result: result + payload}
+                : {...state, result: 0}
+        }
+
+        case "RESET": {
+            return {result: 0}
+        }
+
+        case "SUBMIT_NUMBER": {
+
+            if (isNaN(inputValue) || payload === '') {
+                return {...state, showError: true}
+            }
+
+            return result + inputValue > 0
+                ? {...state, result: result + inputValue, inputValue: ''}
+                : {...state, result: 0, inputValue: ''}
+        }
+
+        case "SET_VALUE_REACT_INPUT": {
+
+            return {...state, inputValue: payload, showError: false}
+        }
+
+        default: {
+            console.log(`ERROR! Action ${type} not found.`)
+            return state
+        }
     }
-    case "RESET": {
-      return {result: 0}
-    }
-    case "SUBMIT_NUMBER": {
-      if (isNaN(inputValue) || payload === '') {
-        return {...state, showError: true}
-      }
-      if(result + inputValue > 0) {
-        return {...state, result: result + inputValue, inputValue: ''}
-      }else {
-        return {...state, result: 0, inputValue: ''}
-      }
-    }
-    case "SET_VALUE_REACT_INPUT": {
-      return {...state, inputValue: payload, showError: false}
-    }
-    default: {
-      console.log(`ERROR! Action ${type} not found.`)
-      return state
-    }
-  }
 }
 
 export default reducer
